3ed36b2d4d61b173cf512f9258cbe5b1fa83a222
gabriellapendo7 <gabriellapendo12@gmail.com>
2024-06-18 13:57:04 +0300

    updated some files

--- app/models/appointment.py
+++
@@ -1,9 +0,0 @@
-# class Appointment:
-#     def __init__(self, id, client_id, date, time):
-#         self.id = id
-#         self.client_id = client_id
-#         self.date = date
-#         self.time = time
-
-#     def __repr__(self):
-#         return f"Appointment(id={self.id}, client_id={self.client_id}, date='{self.date}', time='{self.time}')"
