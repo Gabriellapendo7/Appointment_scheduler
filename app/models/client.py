8b4b225652a6bfe83642e5001b659a11e0cab41c
gabriellapendo7 <gabriellapendo12@gmail.com>
2024-06-18 07:59:36 +0300

    update clients

--- app/models/client.py
+++ app/models/client.py
@@ -1,9 +1,9 @@
-# class Client:
-#     def __init__(self, id, name, email, phone):
-#         self.id = id
-#         self.name = name
-#         self.email = email
-#         self.phone = phone
+class Client:
+    def __init__(self, id, name, email, phone):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.phone = phone
 
-#     def __repr__(self):
-#         return f"Client(id={self.id}, name='{self.name}', email='{self.email}', phone='{self.phone}')"
+    def __repr__(self):
+        return f"Client(id={self.id}, name='{self.name}', email='{self.email}', phone='{self.phone}')"
